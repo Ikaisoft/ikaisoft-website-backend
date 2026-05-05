486ae2024b0c143a1de26f9af249948faa86c9ef
Ikaisoft <ikaisoft10@gmail.com>
2026-05-05 23:17:19 +0530

    temp remove

--- controller/contactController.js
+++ controller/contactController.js
@@ -13,7 +13,7 @@ const sendContactMail = async (req, res) => {
 
     // Send email
     await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: "info@ikaisoft.com",
       to: "info@ikaisoft.com",
       reply_to: email,
       subject: `New Contact Form - ${name}`,
